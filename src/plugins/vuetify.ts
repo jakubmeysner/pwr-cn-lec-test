29162a7c5cafbd6a323c78680b19e5455dfaa510
Jakub Meysner <jakub@jakubmeysner.com>
2024-02-03 20:19:11 +0100

    Change theme primary color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,17 @@
 import "vuetify/styles"
 import { createVuetify } from "vuetify"
 import "@mdi/font/css/materialdesignicons.css"
+import colors from "vuetify/util/colors"
 
 export default createVuetify({
     theme: {
         defaultTheme: "dark",
+        themes: {
+            dark: {
+                colors: {
+                    primary: colors.indigo.accent4,
+                },
+            },
+        },
     },
 })
